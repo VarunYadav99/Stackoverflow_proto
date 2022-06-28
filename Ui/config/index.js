6225fcecbd6e46a070ec278b2b33c959b48d3e87
VarunRaj Badri <54031828+VarunYadav99@users.noreply.github.com>
2022-06-28 16:54:56 +0530

    Add files via upload

---
+++ Ui/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+  BASE_URL: 'http://localhost:5000',
+  // BASE_URL: 'http://' + process.env.BACKEND_SERVICE_NAME + '/' + process.env.BACKEND_SERVICE_PORT,
+};
+
+export default config;
